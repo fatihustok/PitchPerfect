c235a7658fb8f606e862e53374c8368aafb8b105
Refik Fatih Ustok <refikfatihustok@Refiks-MacBook-Air.local>
2016-06-15 17:25:36 +1200

    The project file has been updated to the Swift 2.2 that it prefectly works now

---
+++ Pitch Perfect v2/AppConstants.swift
@@ -0,0 +1,9 @@
+//
+//  AppConstants.swift
+//  Pitch Perfect v2
+//
+//  Created by Refik Fatih Ustok on 15/06/16.
+//  Copyright © 2016 Refik Fatih Ustok. All rights reserved.
+//
+
+import Foundation
